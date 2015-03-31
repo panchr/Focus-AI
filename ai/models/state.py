Added unit tests for Rule.normalize and fixed Numpy.OverflowWarning (for multiplying 32-bit integers, thus causing overflow - converted them to longs)

ai/models/state.py
@@ -129,7 +129,8 @@ class Gamestate(CustomTypeBase):
 	def compare(stateA, stateB):
 		'''Compares two states and returns the quotient of similarity'''
 		difference = np.sum(abs(stateA ^ stateB))
-		maxValue = max(np.max(stateA), np.max(stateB))
-		minValue = min(np.min(stateA), np.min(stateB))
-		arraySize = float(stateA.size) * (maxValue - minValue) # have to convert this to a float to prevent a Numpy overflow
+		# max and min have to be converted to longs or we get an OverflowWarning because they are 32-bit integers (and multiplying them later on will take them over 32-bits)
+		maxValue = long(max(np.max(stateA), np.max(stateB)))
+		minValue = long(min(np.min(stateA), np.min(stateB)))
+		arraySize = (stateA.size) * (maxValue - minValue)
 		return float(arraySize - difference) / arraySize # doesn't account for position in the matrix, which is important

ai/tests/test_models/test_rule.py
@@ -3,13 +3,25 @@
 
 import unittest
 import baseTests
-
 from modelTestBase import ModelTestBase
-from models.rule import Rule
 
-class TestRule(ModelTestBase, unittest.TestCase):
+import random
+import mongokit
+import numpy as np
+
+import models
+import config
+
+def setup():
+	'''Setup the test suite'''
+	conn = mongokit.Connection(host = baseTests.TEST_HOST, port = baseTests.TEST_PORT)
+	conn.register(models.Rule)
+	models.register(conn)
+	TestRule.connection = conn
+
+class TestRule(baseTests.DatabaseTest, ModelTestBase, unittest.TestCase):
 	'''Tests the models.rule.Rule Model'''
-	model = Rule
+	model = models.Rule
 
 	def test_hasIncreaseWeight(self):
 		'''Has function increaseWeight'''
@@ -19,14 +31,35 @@ class TestRule(ModelTestBase, unittest.TestCase):
 		'''Has function decreaseWeight'''
 		self.assertFunctionExists(self.modelObject, "decreaseWeight")
 
+	def test_hasNormalize(self):
+		'''Has method normalize'''
+		self.assertFunctionExists(self.model, "normalize")
+
 	def test_increaseWeight(self):
 		'''increaseWeight works'''
 		currentWeight = self.modelObject.weight
 		self.modelObject.increaseWeight(save = False)
-		self.assertEquals(self.modelObject.weight, currentWeight + 1)
+		self.assertEquals(self.modelObject.weight, currentWeight + config.WEIGHT_DELTA)
 		
 	def test_decreaseWeight(self):
 		'''decreaseWeight works'''
 		currentWeight = self.modelObject.weight
 		self.modelObject.decreaseWeight(save = False)
-		self.assertEquals(self.modelObject.weight, currentWeight - 1)
+		self.assertEquals(self.modelObject.weight, currentWeight - config.WEIGHT_DELTA)
+
+	def test_normalize(self):
+		'''Rule.normalize works'''
+		numTests = 100
+		newArray = lambda: np.zeros((8, 8), dtype = np.int32)
+
+		rules = [models.Rule.new(newArray(), newArray(), [], initialWeight = random.randint(0, numTests)) for i in xrange(numTests)]
+
+		sumWeight = float(sum(map(lambda rule: rule.weight, rules)))
+		normalized = map(lambda rule: rule.weight / sumWeight, rules)
+		
+		models.Rule.normalize()
+
+		for index, rule in enumerate(rules, 0):
+			rule.reload()
+			self.assertEquals(rule.weight, normalized[index])
+			rule.delete()