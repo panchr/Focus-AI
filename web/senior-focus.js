795559effadecaf93df2ee0d18b103922c31a2be
Rushy Panchal <panchal.rushy@gmail.com>
2015-04-08 23:36:04 -0400

    Started main web development aspects. Note: most of the files are not in their final positions and will be removed from the repository (and added to my website's repository) once finished.

---
+++ web/senior-focus.js
@@ -0,0 +1,21 @@
+// Rushy Panchal
+// senior-focus.js
+// Client-side library for my Senior Focus project
+
+function main() {
+	// Main function
+	var socket = createSocketConnection();
+	var board = createGameBoard(8, 8);
+	}
+
+function createSocketConnection() {
+	// Create the main socket connection
+	return null; // not yet implemented
+	}
+
+function togglePlayers() {
+	// Toggles the players
+	$("#online-players-list").slideToggle();
+	}
+
+$(document).ready(main);
