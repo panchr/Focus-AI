ed2adbbb5b6aa63027b69c72d8750b27cd653b7a
Rushy Panchal <panchal.rushy@gmail.com>
2015-04-09 09:44:15 -0400

    Continued designing the main web page. The interaction with the server has not yet been implemented.

--- web/senior-focus.js
+++ web/senior-focus.js
@@ -2,10 +2,10 @@
 // senior-focus.js
 // Client-side library for my Senior Focus project
 
-function main() {
+function initializeFocus() {
 	// Main function
 	var socket = createSocketConnection();
-	var board = createGameBoard(8, 8);
+	board.create(8, 8);
 	}
 
 function createSocketConnection() {
@@ -13,9 +13,56 @@ function createSocketConnection() {
 	return null; // not yet implemented
 	}
 
-function togglePlayers() {
-	// Toggles the players
-	$("#online-players-list").slideToggle();
+var board = {
+	pieces: {},
+	create: function(height, width) {
+		var gameBoard = $("#board");
+		for (h = 0; h < height; h++) {
+			var row = document.createElement("div");
+			row.className = "board-row";
+			for (w = 0; w < width; w++) {
+				var tile = document.createElement("div");
+				tile.className = "board-tile";
+				row.appendChild(tile);
+				}
+			gameBoard.append(row);
+			gameBoard.append(document.createElement("br"));
+			}
+		}
 	}
 
-$(document).ready(main);
+var players = {
+	allPlayers: {},
+	toggleDown: 'ion-ios7-arrow-thin-up',
+	toggleUp: 'ion-ios7-arrow-thin-down',
+	toggle: function() {
+		// Toggle the list of players
+		var players = this;
+		$("#online-players-list").slideToggle(function() {
+			$toggler = $("#player-list-toggler");
+			if ($toggler.hasClass(players.toggleUp)) {
+				$toggler.removeClass(players.toggleUp).addClass(players.toggleDown);
+				}
+			else {
+				$toggler.removeClass(players.toggleDown).addClass(players.toggleUp);
+				}
+			});
+		},
+	add: function(name) {
+		// Add to the list of players
+		var player = document.createElement("li");
+		player.innerHTML = name;
+		player.className = "online-player";
+		$("#online-players-list").append(player);
+		this.allPlayers[name] = player;
+		},
+	remove: function(name) {
+		// Remove from the list of players
+		if (name in this.allPlayers) {
+			$(this.allPlayers[name]).remove();
+			delete this.allPlayers[name];
+			}
+		}
+	};
+
+$(document).ready(initializeFocus);
