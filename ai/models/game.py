7a267d77ba39b3b47b4a9e0f787d994daaa38b8f
Rushy Panchal <panchal.rushy@gmail.com>
2015-04-12 14:00:26 -0400

    Added timestamp field to the Game model, which allows me to see the change in feedback over time

--- ai/models/game.py
+++ ai/models/game.py
@@ -6,6 +6,8 @@ from bson.objectid import ObjectId
 from models.model import Model
 from models.state import Gamestate
 
+import datetime
+
 class Game(Model):
 	'''Contains all of the data about a single game that was played'''
 	__collection__ = "history"
@@ -14,10 +16,11 @@ class Game(Model):
 		"history": [Gamestate()],
 		"rules": [ObjectId], # Rule.id, only needed if AI
 		"feedback": basestring,
-		"seemedHuman": bool
+		"seemedHuman": bool,
+		"timestamp": datetime.datetime
 		}
 
-	required = ["rules", "history", "feedback", "seemedHuman"]
+	required = ["rules", "history", "feedback", "seemedHuman", "timestamp"]
 
 	default_values = {
 		"feedback": "",
@@ -25,3 +28,17 @@ class Game(Model):
 		"history": [],
 		"rules": []
 		}
+
+	@staticmethod
+	def new(history, rules, feedback, seemedHuman):
+		'''Store a new record in the history'''
+		record = Model.connection.Game()
+		record.history = history
+		record.rules = rules
+		record.feedback = feedback
+		record.seemedHuman = seemedHuman
+		record.timestamp = datetime.datetime.now()
+
+		record.save()
+
+		return record
