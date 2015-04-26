3c420bd9afff746ada1396fa91218e50f14bd48b
Rushy Panchal <panchal.rushy@gmail.com>
2015-04-26 14:43:02 -0400

    Search for king moves as well

--- ai/core/database.py
+++ ai/core/database.py
@@ -9,6 +9,8 @@ from models.state import Gamestate
 
 import config
 
+converter = Gamestate()
+
 class Database(mongokit.Connection):
 	'''Specific database wrapper for a MongoDB database'''
 	def getStimuli(self):
@@ -19,11 +21,10 @@ class Database(mongokit.Connection):
 	def getMatchingRules(self, state, stimuli, piece):
 		'''Finds the rules matching the patterns'''
 		if len(stimuli) > 0 and isinstance(stimuli[0], np.ndarray):
-			converter = Gamestate()
 			stimuli = map(converter.to_bson, stimuli)
 		results = list(self.Rule.find({
-			"condition": {"$in": stimuli}, # might need to provide an encoded instance of Gamestate here
-			"piece": {"$in": [piece, 0]},
+			"condition": {"$in": stimuli},
+			"piece": {"$in": [piece, -piece, 0]},
 			}).sort("weight", -1).limit(config.RULE_MATCHES))
 		results.sort(key = lambda item: Gamestate.compare(state, item.state), reverse = True)
 		return results
